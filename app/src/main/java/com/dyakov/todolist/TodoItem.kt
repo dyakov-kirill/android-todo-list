c6714c0926c000f1017c1a2410cf7a24bdd63183
Kirill Dyakov <kira-d03@mail.ru>
2023-07-16 16:26:53 +0300

    Fixed compilation

--- app/src/main/java/com/dyakov/todolist/TodoItem.kt
+++ app/src/main/java/com/dyakov/todolist/TodoItem.kt
@@ -1,7 +1,12 @@
 package com.dyakov.todolist
 
+import java.sql.Time
+
 data class TodoItem(
     val id: String,
     val text: String,
-    val priority: Priority
+    val priority: Priority,
+    val isDone: Boolean,
+    val creationTime: Time,
+    val editTime: Time
 )
